7385c5038c1d20f32565842485fece86eaf69f95
judit Bardon <juditbardonromero@gmail.com>
2024-11-20 18:23:10 +0000

    added Avatar component with photo and username to put on navbar when user is logged in

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,20 @@
+import { UserGreeting } from '../config/types'
+import React = require('react')
+
+type AvatarProps = {
+    user: UserGreeting
+}
+
+function Avatar(props: AvatarProps) {
+
+    const { user } = props
+
+    return (
+        <div className='border-2 border-red-600'>
+            <img src={user.photo} alt="Avatar" />
+            <p>{user.username}</p>
+        </div>
+    )
+}
+
+export default Avatar
