e1f854481044d1620581a23d4d4e4ae1f60af31f
judit Bardon <juditbardonromero@gmail.com>
2025-01-09 19:31:55 +0000

    started home page

--- src/components/ui/Titles.tsx
+++ src/components/ui/Titles.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
 
-function Titles(props) {
+function Titles(props: any) {
 
     const { children } = props
 
     return (
-        <>
-            <h1 className= "text-3xl font-bold text-center mt-4">{props.children}</h1>
-        </>
+        <div className="flex flex-col gap-2 mt-auto items-center 2xl:mt-10">
+            <h1 className='text-xl sm:text-2xl md:text-2xl lg:text-4xl xl:text-5xl text-emerald-700 font-bold font-sans mt-2 lg:mt-4'>{children}</h1>
+        </div>
     )
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,10 @@
-import Titles from "../components/ui/Titles"
+
 
 function HomePage() {
     return (
-        <div>
-            <Titles>Home</Titles>
+        <div className="flex flex-col gap-2 mt-auto pt-8 items-center 2xl:mt-10">
+            <h1 className='text-xl sm:text-2xl md:text-2xl lg:text-4xl xl:text-5xl text-emerald-700 font-bold font-sans mt-2 lg:mt-4'>MyBookshelf</h1>
+            <span className='text-sm sm:text-base md:text-base lg:text-lg xl:text-lg text-gray-700 font-extralight font'>The library that fits in your pocket</span>
         </div>
     )
 }
